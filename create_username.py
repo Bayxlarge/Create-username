e9747636e45ca9b38ec809580a16d44ba649a145
Bayxlarge <105606568+Bayxlarge@users.noreply.github.com>
2022-07-07 12:31:16 +0300

    Add files via upload

---
+++ create_username.py
@@ -0,0 +1,11 @@
+#by bayxlarge
+
+import random
+import time as tm
+1_nickname = ["x", "y"] #here is your nicknames1
+2_nickname = ["b", "a"] #here is your nicknames2
+
+randomly_username = random.choice(1_nickname) + random.choice(2_nickname)
+
+print(randomly_username)
+tm.sleep(5)
